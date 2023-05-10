775cb96ad48018b8d2f3edc752f23723e3c53077
nus3 <hadayota33@gmail.com>
2023-05-10 20:26:35 +0900

    add style

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,28 @@
-import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
+import { Head, asset } from "$fresh/runtime.ts";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Fresh Labs</title>
+        <link rel="stylesheet" href={asset("/style.css")} />
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
-      </div>
+      <header>
+        <h1>Fresh Labs</h1>
+      </header>
+      <main>
+        <ul>
+          <li>
+            <a href="./pip">Picture-in-Picture</a>
+          </li>
+          <li>
+            <a href="./view-transitions-api/">View Transitions API</a>
+          </li>
+          <li>
+            <a href="./opfs/">Origin private file system</a>
+          </li>
+        </ul>
+      </main>
     </>
   );
 }
